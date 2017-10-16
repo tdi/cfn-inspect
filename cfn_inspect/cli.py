1fbf86e2706a8c614e30477ec7f78b668ff79cf4
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2017-10-16 10:14:42 +0200

    Error handling in validation

--- cfn_inspect/cli.py
+++ cfn_inspect/cli.py
@@ -6,6 +6,7 @@ from cfn_flip import to_json
 from .__version__ import __version__
 import boto3
 import sys
+from botocore.exceptions import ValidationError, ClientError
 
 
 def _pprint_resources(t, verbose=False):
@@ -42,19 +43,26 @@ def _pprint_parameters(t, verbose=False):
         else:
             print("  {}".format(p))
 
-
 def _boto_validate(t):
-    cfc = boto3.client("cloudformation")
-    return cfc.validate_template(TemplateBody=t)
-
+    try:
+        cfc = boto3.client("cloudformation")
+    except Exception as e:
+        raise e
+    try:
+        res = cfc.validate_template(TemplateBody=t)
+    except Exception as e:
+        return (False, e.response['Error']['Message'])
+    return (True, res)
 
 def _greeter():
     return crayons.blue("cfn-inspect v{}, Dariusz Dwornikowski".format(__version__), bold=True)
 
+
 @click.command()
 @click.argument("template", type=click.File('r'))
 @click.option("--version", is_flag=True, default=False, help="Show version and exit")
-@click.option("--verbose", "-v", is_flag=True, default=False, help="Be more verbose about the output")
+@click.option(
+    "--verbose", "-v", is_flag=True, default=False, help="Be more verbose about the output")
 @click.option("--validate", is_flag=True, default=False, help="Validate template with AWS")
 def cli(template, verbose=False, validate=False, version=False):
     click.echo(_greeter(), err=True)
@@ -64,6 +72,7 @@ def cli(template, verbose=False, validate=False, version=False):
     print("{}: {}".format(
         crayons.white("Inspecting template", bold=True), crayons.blue(template.name)))
     template = template.read()
+
     try:
         t = json.loads(template)
     except Exception as e:
@@ -72,7 +81,9 @@ def cli(template, verbose=False, validate=False, version=False):
         json_template = to_json(template)
         t = json.loads(json_template)
     except Exception as e:
-        raise Exception("Could not determine the input format: {}", e)
+        click.echo(
+            "Could not determine the input format or format not sane: {}".format(e), err=True)
+        sys.exit(1)
 
     if 'Description' in t:
         print("{}: {}".format(
@@ -88,15 +99,20 @@ def cli(template, verbose=False, validate=False, version=False):
 
     if validate:
         if len(template) > 51200:
-            click.echo(crayons.red("Can't validate with AWS - template size exceeds 51200 bytes"))
+            click.echo(
+                crayons.red("Can't validate the template AWS - template size exceeds 51200 bytes"),
+                err=True)
             sys.exit(1)
         try:
             result = _boto_validate(template)
-            if 'Description' in result:
+            if result[0] == True:
                 print(crayons.cyan("Yay ! template is valid", bold=True))
+            else:
+                print(crayons.cyan(":(, template is not valid: {}".format(result[1]), bold=True))
         except Exception as e:
-            click.echo(crayons.red("AWS credentials not working, {}".format(e)), err=True)
+            click.echo(crayons.red("Problem with boto3 connection, {}".format(e)), err=True)
             sys.exit(1)
+    sys.exit(0)
 
 
 if __name__ == "__main__":
